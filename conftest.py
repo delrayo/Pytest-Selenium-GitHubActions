96872b47bcdd560d9dc4357f33a357b6c7a545c4
Vladimir Shkodin <v.s.shkodin@gmail.com>
2022-08-18 20:16:43 -0700

    vlad-changes

--- conftest.py
+++ conftest.py
@@ -2,29 +2,29 @@ import pytest
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.utils import ChromeType
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 
 
 @pytest.fixture()
 def setup(request):
-    chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install())
-
     chrome_options = Options()
     options = [
-    "--headless",
-    "--disable-gpu",
-    "--window-size=1920,1200",
-    "--ignore-certificate-errors",
-    "--disable-extensions",
-    "--no-sandbox",
-    "--disable-dev-shm-usage"
-]
+        "--headless",
+        "--disable-gpu",
+        "--window-size=1920,1200",
+        "--ignore-certificate-errors",
+        "--disable-extensions",
+        "--no-sandbox",
+        "--disable-dev-shm-usage"
+    ]
     for option in options:
         chrome_options.add_argument(option)
 
-    request.cls.driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
-
-
+    request.cls.driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()),
+        options=chrome_options)
     yield request.cls.driver
     request.cls.driver.close()
--- test_web01.py
+++ test_web01.py
@@ -4,10 +4,6 @@ import pytest
 @pytest.mark.usefixtures("setup")
 class TestExampleOne:
     def test_title(self):
-        self.driver.get('https://www.delrayo.tech')
-        assert self.driver.title == "DelRayo.tech - Delrayo Tech"
-    
-    def test_title_blog(self):
-        self.driver.get('https://www.delrayo.tech/blog')
-        print(self.driver.title)
+        self.driver.get('https://vshkodin.com/')
+        assert self.driver.title == "Vladimir Shkodin"
         
