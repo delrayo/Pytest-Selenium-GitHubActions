9dd2ad01ff8cdbfb1c3446a39d5e793c5ae8a298
Ricardo <ricardo.delrayo@originatorkids.com>
2023-12-12 19:20:41 -0600

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,10 +1,26 @@
 import pytest
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 
 
 @pytest.fixture()
 def setup(request):
-    
+
+    chrome_options = Options()
+    options = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
+]
+    for option in options:
+        chrome_options.add_argument(option)
+
+    request.cls.driver = webdriver.Chrome(options=chrome_options)
     
 
     request.cls.driver = webdriver.Chrome()
