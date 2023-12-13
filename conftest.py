b658ade40695d50ebafcade41fdff0dc32ea02fb
Ricardo <ricardo.delrayo@originatorkids.com>
2023-12-12 18:45:05 -0600

    fixed tests
    removed webdrivermanager and took advantage of the selenium driver included on selenium 4

--- conftest.py
+++ conftest.py
@@ -1,30 +1,12 @@
 import pytest
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.utils import ChromeType
 from selenium import webdriver
 
 
 @pytest.fixture()
 def setup(request):
-    chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install())
+    
+    
 
-    chrome_options = Options()
-    options = [
-    "--headless",
-    "--disable-gpu",
-    "--window-size=1920,1200",
-    "--ignore-certificate-errors",
-    "--disable-extensions",
-    "--no-sandbox",
-    "--disable-dev-shm-usage"
-]
-    for option in options:
-        chrome_options.add_argument(option)
-
-    request.cls.driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
-
-
+    request.cls.driver = webdriver.Chrome()
     yield request.cls.driver
     request.cls.driver.close()
--- test_web01.py
+++ test_web01.py
@@ -4,7 +4,7 @@ import pytest
 @pytest.mark.usefixtures("setup")
 class TestExampleOne:
     def test_title(self):
-        self.driver.get('https://www.delrayo.tech')
+        self.driver.get("https://www.delrayo.tech")
         assert self.driver.title == "DelRayo.tech - Delrayo Tech"
     
     def test_title_blog(self):
